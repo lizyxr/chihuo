f2aa9d366c0a6ab88744f65e762f7ae17d309821
Xinrui (Liz) <ee07b282@gmail.com>
2015-10-06 03:02:33 -0700

    Update tableAddEntry.js

--- stylesheets/tableAddEntry.js
+++ stylesheets/tableAddEntry.js
@@ -1,13 +1,4 @@
 /*$(document).ready(function(){
-  $(".button").click(function () {
-    $("#sForm").toggleClass("open");   
-  });
-  $(".controlTd").click(function () {
-    $(this).children(".settingsIcons").toggleClass("display"); 
-    $(this).children(".settingsIcon").toggleClass("openIcon"); 
-  });
-});*/
-$(document).ready(function(){
   function updateText(event){
     var input=$(this);
     setTimeout(function(){
@@ -18,8 +9,17 @@ $(document).ready(function(){
         input.parent().removeClass("floating-placeholder-float");
     },1)
   }
+  $(".button").click(function () {
+    $("#sForm").toggleClass("open");   
+  });
+  $(".controlTd").click(function () {
+    $(this).children(".settingsIcons").toggleClass("display"); 
+    $(this).children(".settingsIcon").toggleClass("openIcon"); 
+  });
   $(".floating-placeholder input").keydown(updateText);
   $(".floating-placeholder input").change(updateText);
+});*/
+$(document).ready(function(){
   $(".table-add").click(function () { 
     var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
     $TABLE.find('table').append($clone);
