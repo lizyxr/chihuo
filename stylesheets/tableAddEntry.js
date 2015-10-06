086142da8c456ab2b2af20f2d9dc5985921a5e2f
Xinrui (Liz) <ee07b282@gmail.com>
2015-10-06 02:11:54 -0700

    Update tableAddEntry.js

--- stylesheets/tableAddEntry.js
+++ stylesheets/tableAddEntry.js
@@ -2,6 +2,20 @@ $(document).ready(function(){
   $(".button").click(function () {
     $("#sForm").toggleClass("open");   
   });
+$(document).ready(function(){
+  function updateText(event){
+    var input=$(this);
+    setTimeout(function(){
+      var val=input.val();
+      if(val!="")
+        input.parent().addClass("floating-placeholder-float");
+      else
+        input.parent().removeClass("floating-placeholder-float");
+    },1)
+  }
+  $(".floating-placeholder input").keydown(updateText);
+  $(".floating-placeholder input").change(updateText);
+});
 
   $(".controlTd").click(function () {
     $(this).children(".settingsIcons").toggleClass("display"); 
